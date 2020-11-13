684eb81dd6d96e118cd011fc9349becdf6df6520
TheDayG0ne <evstr2006@mail.ru>
2020-11-13 16:06:40 +0700

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-html = '<html><body><h3>Hello from the Raspberry Pi</h3><img src="raspberrypi.jpg"/></body></html>'
+html = '<html><body><h3>Hello from the Raspberry Pi</h3><img src="650.png"/></body></html>'
 
 class ServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -20,8 +20,27 @@ def server_thread(port):
     except KeyboardInterrupt:
         pass
     httpd.server_close()
+    
+    def do_GET(self):
+        print("GET request, Path:", self.path)
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(html.encode('utf-8'))
+        elif self.path.endswith(".png"):
+            self.send_response(200)
+            self.send_header('Content-type', 'image/jpg')
+            self.end_headers()
+            with open(os.curdir + os.sep + self.path, 'rb') as file:
+                self.wfile.write(file.read())
+        else:
+            self.send_error(404, "Page Not Found {}".format(self.path))
+
 
 if __name__ == '__main__':
     port = 8000
     print("Starting server at port %d" % port)
     server_thread(port)
+
+
